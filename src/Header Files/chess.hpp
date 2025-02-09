e8e54865ac2157625a50f31aff4182a4f8ca6746
noobOriginals <noobOriginals@xx.com>
2025-02-09 14:16:38 +0200

    Started reformating

--- src/Header Files/chess.hpp
+++ src/Header Files/chess.hpp
@@ -1,48 +1,60 @@
-#pragma once
 #ifndef CHESS_H
+#define CHESS_H
 
 #include <iostream>
 #include <string>
+#include <chrono>
 
-#define WHITE 8
-#define BLACK 16
+#define White 8
+#define Black 16
 
-#define NONE 0
-#define PAWN 1
-#define KNIGHT 2
-#define BISHOP 3
-#define ROOK 4
-#define QUEEN 5
-#define KING 6
+#define None 0
+#define Pawn 1
+#define Knight 2
+#define Bishop 3
+#define Rook 4
+#define Queen 5
+#define King 6
 
 namespace chess {
-    class Board {
-    public:
-        Board();
-        Board(std::string fen);
-        void reset();
-        void reset(std::string fen);
-        void getSquare(std::string sqr, int& r, int& c);
-        unsigned char getPiece(char value);
-        char getValue(unsigned char piece);
-        void print();
-        bool castleRights(int idx) { return castle[idx]; }
-        void enPassantSquare(int& r, int& c) { r = enr; c = enc; }
-        bool whiteToMove() { return whiteMove; }
-        int halfMoves() { return halfm; }
-        int fullMoves() { return fullm; }
-        unsigned char* operator[](int i) {
-            return board[i];
-        }
-    private:
-        unsigned char board[8][8];
-        bool castle[4] = { true };
-        bool whiteMove;
-        int enr, enc;
-        int halfm;
-        int fullm;
-        void printRow();
-    };
+
+typedef uint8_t Piece;
+typedef Move* Moves;
+
+long nanoTime();
+
+namespace piece {
+    Piece getPieceColor(Piece value);
+    Piece getPieceType(Piece value);
+    bool isColor(Piece value, Piece color) ;
+    bool isType(Piece value, Piece type);
+    char toChar(Piece value);
+    Piece toPiece(char value);
+}
+struct Move {
+    int startSquare, endSquare;
+    Move();
+    Move(Move& m);
+    void operator=(Move& move);
+    Move(int startSquare, int endSquare);
+    Move(std::string move);
+    Move(std::string startSquare, std::string endSquare);
+    std::string toString();
+    static Move fromEnPassantTargetSquare(std::string targetSquare);
+};
+namespace board {
+    Piece square[64] = {};
+    bool castleRights[4] = {};
+    bool whiteToMove = true;
+    Move lastMove;
+    uint32_t halfMoves = 0, fullMoves = 0;
+    uint32_t whitePieces[16] = {}, blackPieces[16] = {};
+    void reset(std::string fen);
+    void reset();
+    Piece** to2DArray();
+    Moves generateLegalMoves();
+}
+
 }
 
 #endif
--- src/Source Files/chess.cpp
+++ src/Source Files/chess.cpp
@@ -1,185 +1,118 @@
 #include <chess.hpp>
+
 using namespace std;
 
-bool pieceIsColor(unsigned char piece, unsigned char color) {
-    return (piece >> 3 == color >> 3);
+namespace chess {
+
+constexpr char* alphabet = "abcdefgh";
+
+long nanoTime() {
+    auto now = std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now());
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count();
 }
 
-namespace chess {
-    Board::Board() {
-        reset();
+namespace piece {
+    Piece getPieceColor(Piece value) {
+        value = (Piece) (value >> 3);
+        return (Piece) (value << 3);
     }
-    Board::Board(string fen) {
-        reset(fen);
+    Piece getPieceType(Piece value) {
+        value = (Piece) (value | 24);
+        return (Piece) (value ^ 24);
     }
-    void Board::reset() {
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                board[i][j] = 0;
-            }
-        }
-        whiteMove = true;
-        board[0][0] = BLACK | ROOK;
-        board[0][1] = BLACK | KNIGHT;
-        board[0][2] = BLACK | BISHOP;
-        board[0][3] = BLACK | QUEEN;
-        board[0][4] = BLACK | KING;
-        board[0][5] = BLACK | BISHOP;
-        board[0][6] = BLACK | KNIGHT;
-        board[0][7] = BLACK | ROOK;
-        for (int i = 0; i < 8; i++) {
-            board[1][i] = BLACK | PAWN;
-            board[6][i] = WHITE | PAWN;
-        }
-        board[7][0] = WHITE | ROOK;
-        board[7][1] = WHITE | KNIGHT;
-        board[7][2] = WHITE | BISHOP;
-        board[7][3] = WHITE | QUEEN;
-        board[7][4] = WHITE | KING;
-        board[7][5] = WHITE | BISHOP;
-        board[7][6] = WHITE | KNIGHT;
-        board[7][7] = WHITE | ROOK;
+    bool isColor(Piece value, Piece color) {
+        return getPieceColor(value) == color;
     }
-    void Board::reset(string fen) {
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                board[i][j] = 0;
-            }
-        }
-        for (int i = 0; i < 4; i++) {
-            castle[i] = false;
-        }
-        char c;
-        int s = 0;
-        int last = 0;
-        for (int i = 0; i < fen.length(); i++) {
-            c = fen[i];
-            if (c < 58 && c > 47) {
-                string st(1, c);
-                s += stoi(st);
-            } else if (c == '/') {
-                s += (s % 8 == 0) ? 0 : 8 - (s % 8);
-            } else {
-                board[(int)(s / 8)][s % 8] = getPiece(c);
-                s++;
-            }
-            if (s > 63) {
-                last = i;
-                break;
-            }
-        }
-        last += 2;
-        c = fen[last];
-        whiteMove = (c == 'w');
-        last += 2;
-        c = fen[last];
-        string cas = fen.substr(last, fen.find_first_of(' ', last));
-        if (cas.length() > 0) {
-            last = fen.find_first_of(' ', last);
-            for (int i = 0; i < cas.length(); i++) {
-                c = cas[i];
-                switch (c) {
-                    case 'K':
-                        castle[0] = true;
-                        break;
-                    case 'Q':
-                        castle[1] = true;
-                        break;
-                    case 'k':
-                        castle[2] = true;
-                        break;
-                    case 'q':
-                        castle[3] = true;
-                        break;
-                }
-            }
-        }
-        last++;
-        c = fen[last];
-        if (c != '-') {
-            string en = fen.substr(last, fen.find_first_of(' ', last));
-            last = fen.find_first_of(' ', last);
-            getSquare(en, enr, enc);
-        } else {
-            last = fen.find_first_of(' ', last);
-            enr = 0;
-            enc = 0;
-        }
-        last++;
-        string hm = fen.substr(last, fen.find_first_of(' ', last));
-        halfm = stoi(hm);
-        last = fen.find_first_of(' ', last) + 1;
-        string fm = fen.substr(last);
-        fullm = stoi(fm);
+    bool isType(Piece value, Piece type) {
+        return getPieceType(value) == type;
     }
-    void Board::getSquare(string sqr, int& r, int& c) {
-        string alph = "abcdefgh";
-        c = alph.find_first_of(sqr[0]);
-        r = 8 - stoi(string(1, sqr[1]));
-    }
-    unsigned char Board::getPiece(char value) {
-        bool uppercase = (value < 91);
-        value = toupper(value);
+    char toChar(Piece value) {
+        bool uppercase = isColor(value, White);
+        value = getPieceType(value);
         switch (value) {
-            case ' ':
-                return (unsigned char) (((uppercase) ? WHITE : BLACK) | NONE);
-            case 'P':
-                return (unsigned char) (((uppercase) ? WHITE : BLACK) | PAWN);
-            case 'N':
-                return (unsigned char) (((uppercase) ? WHITE : BLACK) | KNIGHT);
-            case 'B':
-                return (unsigned char) (((uppercase) ? WHITE : BLACK) | BISHOP);
-            case 'R':
-                return (unsigned char) (((uppercase) ? WHITE : BLACK) | ROOK);
-            case 'Q':
-                return (unsigned char) (((uppercase) ? WHITE : BLACK) | QUEEN);
-            case 'K':
-                return (unsigned char) (((uppercase) ? WHITE : BLACK) | KING);
-            default:
-                return (unsigned char) (((uppercase) ? WHITE : BLACK) | NONE);
+            case None: return ' ';
+            case Pawn: return uppercase ? 'P' : 'p';
+            case Knight: return uppercase ? 'N' : 'n';
+            case Bishop: return uppercase ? 'B' : 'b';
+            case Rook: return uppercase ? 'R' : 'r';
+            case Queen: return uppercase ? 'Q' : 'q';
+            case King: return uppercase ? 'K' : 'k';
+            default: return ' ';
         }
     }
-    char Board::getValue(unsigned char piece) {
-        bool uppercase = pieceIsColor(piece, WHITE);
-        short add = 3 << 3;
-        piece = (short) (piece | add);
-        piece = (short) (piece ^ add);
-        switch (piece) {
-            case NONE:
-                return (uppercase) ? ' ' : ' ';
-            case PAWN:
-                return (uppercase) ? 'P' : 'p';
-            case KNIGHT:
-                return (uppercase) ? 'N' : 'n';
-            case BISHOP:
-                return (uppercase) ? 'B' : 'b';
-            case ROOK:
-                return (uppercase) ? 'R' : 'r';
-            case QUEEN:
-                return (uppercase) ? 'Q' : 'q';
-            case KING:
-                return (uppercase) ? 'K' : 'k';
-            default:
-                return ' ';
+    Piece toPiece(char value) {
+        bool uppercase = isupper(value);
+        switch (toupper(value)) {
+            case ' ': return None;
+            case 'P': return (Piece) ((uppercase ? White : Black) | Pawn);
+            case 'N': return (Piece) ((uppercase ? White : Black) | Knight);
+            case 'B': return (Piece) ((uppercase ? White : Black) | Bishop);
+            case 'R': return (Piece) ((uppercase ? White : Black) | Rook);
+            case 'Q': return (Piece) ((uppercase ? White : Black) | Queen);
+            case 'K': return (Piece) ((uppercase ? White : Black) | King);
+            default: return None;
         }
     }
-    void Board::printRow() {
-        cout << "+---+---+---+---+---+---+---+---+\n";
+}
+
+Move::Move() {
+    startSquare = 0;
+    endSquare = 0;
+}
+Move::Move(Move& move) {
+    this->startSquare = move.startSquare;
+    this->endSquare = move.endSquare;
+}
+void Move::operator=(Move& move) {
+    this->startSquare = move.startSquare;
+    this->endSquare = move.endSquare;
+}
+Move::Move(int start, int end) {
+    startSquare = start;
+    endSquare = end;
+}
+Move::Move(string move) {
+    int rank, file;
+    rank = 8 - stoi(string(1, move[1]));
+    file = string(alphabet).find_first_of(move[0]);
+    startSquare = rank * 8 + file;
+    rank = 8 - stoi(string(1, move[3]));
+    file = string(alphabet).find_first_of(move[2]);
+    endSquare = rank * 8 + file;
+}
+Move::Move(string start, string end) {
+    string move = start + end;
+    int rank, file;
+    rank = 8 - stoi(string(1, move[1]));
+    file = string(alphabet).find_first_of(move[0]);
+    startSquare = rank * 8 + file;
+    rank = 8 - stoi(string(1, move[3]));
+    file = string(alphabet).find_first_of(move[2]);
+    endSquare = rank * 8 + file;
+}
+std::string Move::toString() {
+    string move = "";
+    string alph = "abcdefgh";
+    int rank, file;
+    file = startSquare % 8;
+    rank = 8 - (startSquare - file) / 8;
+    move += "" + alph[file];
+    move += "" + rank;
+    file = endSquare % 8;
+    rank = 8 - (endSquare - file) / 8;
+    move += "" + alph[file];
+    move += "" + rank;
+    return move;
+}
+Move Move::fromEnPassantTargetSquare(std::string targetSquare) {
+    string file = string(1, targetSquare[0]);
+    Move move;
+    if (targetSquare[1] == '3') {
+        move = Move(file + "2" + file + "4");
+    } else {
+        move = Move(file + "7" + file + "5");
     }
-    void Board::print() {
-        printRow();
-        cout << "|";
-        for (int i = 0; i < 64; i++) {
-            cout << " " << getValue(board[(int)(i / 8)][i % 8]) << " |";
-            if ((i + 1) % 8 == 0) {
-                cout << " " << (9 - (int) ((i + 1) / 8)) << "\n";
-                printRow();
-                if (i == 63) {
-                    break;
-                }
-                cout << "|";
-            }
-        }
-        cout << "  a   b   c   d   e   f   g   h  \n";
-    }
+    return move;
+}
+
 }
