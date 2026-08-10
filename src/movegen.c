d5afa720313383c8f72a51cd3d84af7e2e0083c5
noobOriginals <noobOriginals@xx.com>
2026-08-10 15:55:48 +0300

    Added en passant. Castle is the last on the list.

--- src/movegen.c
+++ src/movegen.c
@@ -122,72 +122,6 @@ void unpackDoublePushes(Bitboard pushes, i8 dist, Bitboard pinned, Bitboard pinM
     POP_STACK_TRACE();
 }
 
-void unpackStandardAttacks(u8 from, Bitboard attacks, Bitboard attackers, Move* moves, u32* count) {
-
-    PUSH_STACK_TRACE("unpackStandardAttacks()");
-
-    Bitboard lsb;
-    u8 to, flag;
-    u32 begin = *count;
-    while (attacks) {
-        to = popToLSB(&attacks, &lsb);
-        flag = lsb & attackers ? MOVE_CAPTURE : MOVE_QUIET;
-        moves[begin++] = (Move) from | ((Move) to << 6) | ((Move) flag << 12);
-    }
-    *count = begin;
-
-    POP_STACK_TRACE();
-}
-
-void unpackPawnAttacks(u8 from, Bitboard attacks, Move* moves, u32* count) {
-
-    PUSH_STACK_TRACE("unpackPawnAttacks()");
-
-    Bitboard lsb;
-    u8 to;
-    Move base;
-    u32 begin = *count;
-    while (attacks) {
-        to = popToLSB(&attacks, &lsb);
-        base = (Move) from | ((Move) to << 6);
-        if (lsb & (RANK_1 | RANK_8)) {
-            moves[begin++] = base | ((Move) MOVE_PROMO_CAPTURE_N << 12);
-            moves[begin++] = base | ((Move) MOVE_PROMO_CAPTURE_B << 12);
-            moves[begin++] = base | ((Move) MOVE_PROMO_CAPTURE_R << 12);
-            moves[begin++] = base | ((Move) MOVE_PROMO_CAPTURE_Q << 12);
-            continue;
-        }
-        moves[begin++] = base | ((Move) MOVE_CAPTURE << 12);
-    }
-    *count = begin;
-
-    POP_STACK_TRACE();
-}
-
-void unpackPawnPushes(Bitboard pushes, i8 dist, Bitboard pinned, Bitboard pinMask[64], Move* moves, u32* count) {
-
-    PUSH_STACK_TRACE("unpackStandardAttacks()");
-
-    Bitboard lsb;
-    u8 from, to, flag;
-    u32 begin = *count;
-    while (pushes) {
-        to = popToLSB(&pushes, &lsb);
-        from = to - dist;
-        flag = abs(dist) > 8 ? MOVE_DOUBLE_PUSH : MOVE_QUIET;
-
-        // Check for pins
-        if (pinned & bbsq(from) && ~pinMask[from] & lsb) {
-            continue;
-        }
-
-        moves[begin++] = (Move) from | ((Move) to << 6) | ((Move) flag << 12);
-    }
-    *count = begin;
-
-    POP_STACK_TRACE();
-}
-
 // Main movegen method
 Move* getLegalMoves(Board* board, Move* moves, u32* count) {
 
@@ -201,12 +135,13 @@ Move* getLegalMoves(Board* board, Move* moves, u32* count) {
     Bitboard defenders = board->bb[side], notDefenders = ~defenders;
     Bitboard attackers = board->bb[opp], notAttackers = ~attackers;
     Bitboard king = board->bb[KING | side], notKing = ~king, allExceptKing = board->all & notKing;
+    Bitboard epPawn = side ? board->epTarget << 8 : board->epTarget >> 8;
     u8 kingsq = (u8) ctzll(king);
     i8 pushDist = side ? -8 : 8;
 
     // Declare useful variables
     Bitboard lsb = 0ull, attacks = 0ull, target = 0ull, bb1 = 0ull, bb2 = 0ull;
-    u8 square = 0u, piece = 0u, i0 = 0u, i1 = 0u, i2 = 0u;
+    u8 square = 0u, piece = 0u, sq2 = 0u;
 
     // Compute
     Bitboard checkers = 0ull, oppSliders = 0ull, attacked = 0ull, pinned = 0ull, pinMask[64];
@@ -248,7 +183,8 @@ Move* getLegalMoves(Board* board, Move* moves, u32* count) {
     // Double check only allows king moves
     if (popcountll(checkers) > 1) {
         attacks = getKingAttacks(kingsq) & ~attacked & notDefenders;
-        unpackStandardAttacks(kingsq, attacks, attackers, moves, count);
+        unpackQuietMoves(kingsq, attacks & notAttackers, moves, count);
+        unpackCaptures(kingsq, attacks & attackers, moves, count);
 
         POP_STACK_TRACE();
 
@@ -278,29 +214,65 @@ Move* getLegalMoves(Board* board, Move* moves, u32* count) {
         }
 
         // Special unpack for pawns
-        // Needs extra promotion check (last rank reached)
-        // But pawn attacks must always be captures, so that check is no longer needed
+        // Needs extra promotion unpack (last rank reached)
+        // But pawn attacks must always be captures, so the quiet unpack is no longer needed
         if (piece == PAWN) {
-            // unpackPawnAttacks(square, attacks & pinMask[square], moves, count);
             unpackCaptures(square, attacks & pinMask[square] & ~(RANK_1 | RANK_8), moves, count);
             unpackPromoCaptures(square, attacks & pinMask[square] & (RANK_1 | RANK_8), moves, count);
             continue;
         }
 
         // Unpack into move array
-        // unpackStandardAttacks(square, attacks & pinMask[square], attackers, moves, count);
         unpackQuietMoves(square, attacks & pinMask[square] & notAttackers, moves, count);
         unpackCaptures(square, attacks & pinMask[square] & attackers, moves, count);
     }
 
-    Bitboard singlePush = getPawnPushes(board->bb[PAWN | side], board->all, side);
-    Bitboard doublePush = getPawnPushes(singlePush & (side ? RANK_6 : RANK_3), board->all, side);
-    singlePush &= target;
-    // unpackPawnPushes(singlePush, side ? -8 : 8, pinned, pinMask, moves, count);
-    // unpackPawnPushes(doublePush & target, side ? -16 : 16, pinned, pinMask, moves, count);
-    unpackPushes(singlePush & ~(RANK_1 | RANK_8), pushDist, pinned, pinMask, moves, count);
-    unpackPromoPushes(singlePush & (RANK_1 | RANK_8), pushDist, pinned, pinMask, moves, count);
-    unpackDoublePushes(doublePush & target, pushDist * 2, pinned, pinMask, moves, count);
+    // Compute pushes
+    bb1 = getPawnPushes(board->bb[PAWN | side], board->all, side);
+    bb2 = getPawnPushes(bb1 & (side ? RANK_6 : RANK_3), board->all, side);
+    bb1 &= target;
+    unpackPushes(bb1 & ~(RANK_1 | RANK_8), pushDist, pinned, pinMask, moves, count);
+    unpackPromoPushes(bb1 & (RANK_1 | RANK_8), pushDist, pinned, pinMask, moves, count);
+    unpackDoublePushes(bb2 & target, pushDist * 2, pinned, pinMask, moves, count);
+
+    // Castle magic
+    bb1 = side ? BLACK_KC | BLACK_QC : WHITE_KC | WHITE_QC;
+
+    // En passant magic
+    if (target & epPawn) {
+        // epTarget's opposite side attacks are exactly the places where friendly pawns should be
+        piece = (u8) ctzll(board->epTarget);
+        attacks = getPawnAttacks(piece, opp) & board->bb[PAWN | side];
+        while (attacks) {
+            // Get square of friendly pawn
+            square = popToLSB(&attacks, &lsb);
+
+            // XOR mask for simulating the ep move. Flips the friendly pawn, the opponent pawn and the epTarget (friendly pawn after being moved)
+            bb1 = lsb | epPawn | board->epTarget;
+            target = board->all ^ bb1; // Reuse variable, the new board occupancy
+            attacked = 0ull; // Reset attacked squares
+
+            // Loop through each opponent slider's attacks (computed earlier)
+            bb1 = oppSliders;
+            while (bb1) {
+                sq2 = popLSB(&bb1);
+                switch (ptype(board->mailbox[sq2])) {
+                    case BISHOP: attacked |= getBishopAttacks(sq2, target); break;
+                    case ROOK: attacked |= getRookAttacks(sq2, target); break;
+                    case QUEEN: attacked |= getQueenAttacks(sq2, target); break;
+                }
+            }
+
+            // If king is attacked, move is not legal
+            if (attacked & king) {
+                continue;
+            }
+
+            // Store move
+            moves[*count] = (Move) square | ((Move) piece << 6) | ((Move) MOVE_EP_CAPTURE << 12);
+            *count += 1;
+        }
+    }
 
     POP_STACK_TRACE();
 
